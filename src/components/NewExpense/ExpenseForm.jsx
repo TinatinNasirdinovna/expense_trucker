3279baf74dff660069a01972de5c355501050012
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2024-06-19 11:06:46 +0600

    take input values

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ExpenseForm = () => {
+    const [inputValue, setInputValue] = useState({
+        title: '',
+        amount: '',
+        date: ''
+    })
+
+    const inputChangeHandler = (event) => {
+        let currentValue = event.target.name
+        setInputValue({
+            ...inputValue,
+            [currentValue]: event.target.value
+        })
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log(inputValue.title);
+        console.log(inputValue.amount);
+        console.log(inputValue.date);
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
            <div className="expenseForm">
             <div className="expenseForm--input">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" name='title' onChange={inputChangeHandler}/>
             </div>
             <div className="expenseForm--input">
                 <label>Amout</label>
-                <input type="number" />
+                <input type="number" name='amount' onChange={inputChangeHandler}/>
             </div>
             <div className="expenseForm--input">
                 <label>Date</label>
-                <input type="date" />
+                <input type="date" name='date' onChange={inputChangeHandler}/>
             </div>
            </div>
            <div className="expenseForm--btn">
