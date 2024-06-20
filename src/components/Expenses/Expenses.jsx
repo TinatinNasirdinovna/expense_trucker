b11022843cdc573c3abbea993a4decb02a7c46d4
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2024-06-20 19:15:01 +0600

    two-data-binding

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from '../NewExpense/ExpenseFilter';
 
 const Expenses = (props) => {
+    const [filteredData, setFilteredData] = useState('2024')
+
+    const changeYear = (year) => {
+        console.log(year);
+        setFilteredData(year)
+    }
+
+    const filteredExpense = props.expenses.filter(el => {
+        return el.date.getFullYear().toString() === filteredData
+    })
+
     return (
         <Card className='expenses'>
-            {props.expenses.map(el => (
+            <ExpenseFilter onGetYear={changeYear} selected={filteredData}/>
+            {filteredExpense.map(el => (
                 <ExpenseItem key={el.id} title={el.title} amount={el.amount} date={el.date}/>
             ))}
         </Card>
---
+++ src/components/NewExpense/ExpenseFilter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ExpenseFilter = (props) => {
+
+    const getGhangedValue = (e) => {
+        props.onGetYear(e.target.value)
+    }
+    return (
+        <div className='expenseFilter'>
+            <div className="expenseFilter--control">
+                <label>Filter by year</label>
+                <select name="" id="" onChange={getGhangedValue} value={props.selected}>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpenseFilter;
