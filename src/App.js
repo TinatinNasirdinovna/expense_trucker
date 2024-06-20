66fb45a9b6f28f877ebb04048fab94b78cc38cd5
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2024-06-20 18:45:58 +0600

    two-data-binding select and inputs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
+import { useState } from "react";
 import "./App.scss";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const initData = [
+  {
+    id: "el1",
+    title: "Toilet Paper",
+    amount: 200,
+    date: new Date(2024, 7, 10),
+  },
+  {
+    id: "el2",
+    title: "Car Insuranse",
+    amount: 400,
+    date: new Date(2026, 8, 2),
+  },
+  {
+    id: "el3",
+    title: "Utilies",
+    amount: 100,
+    date: new Date(2025, 3, 2),
+  },
+  {
+    id: "el4",
+    title: "New TV",
+    amount: 500,
+    date: new Date(2023, 1, 2),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "el1",
-      title: "Toilet Paper",
-      amount: 200,
-      date: new Date(2024, 7, 10),
-    },
-    {
-      id: "el2",
-      title: "Car Insuranse",
-      amount: 400,
-      date: new Date(2024, 8, 2),
-    },
-    {
-      id: "el3",
-      title: "Utilies",
-      amount: 100,
-      date: new Date(2024, 3, 2),
-    },
-    {
-      id: "el4",
-      title: "New TV",
-      amount: 500,
-      date: new Date(2024, 1, 2),
-    },
-  ];
+  const [expenses, setExpenses] = useState(initData)
+
+  const getExpense = (data) => {
+    setExpenses([data,...expenses])
+  }
+
   return (
     <div className="container">
-      <NewExpense/>
+      <NewExpense onGet={getExpense}/>
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState('')
+    const [date, setDate] = useState('')
+    
+    const titleChangeHandler = (e) => {
+        setTitle(e.target.value)
+    }    
+
+    const amountChangHandler = (e) => {
+        setAmount(e.target.value)
+    }
+
+    const dateChangHandler = (e) => {
+        setDate(e.target.value)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const data = {
+            title, 
+            amount,
+            date: new Date(date)
+        }
+        props.onGet(data)
+        setTitle('')
+        setAmount('')
+        setDate('')
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
            <div className="expenseForm">
             <div className="expenseForm--input">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" onChange={titleChangeHandler} value={title}/>
             </div>
             <div className="expenseForm--input">
                 <label>Amout</label>
-                <input type="number" />
+                <input type="number" onChange={amountChangHandler} value={amount} />
             </div>
             <div className="expenseForm--input">
                 <label>Date</label>
-                <input type="date" />
+                <input type="date" onChange={dateChangHandler} value={date}/>
             </div>
            </div>
            <div className="expenseForm--btn">
-            <button>Add Expense</button>
+            <button type='submit'>Add Expense</button>
            </div>
         </form>
     );
